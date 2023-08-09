6c1274652b3f114bd35b7fb6d05f6d145406d563
Ampaw24 <ampawsafo22@gmail.com>
2023-08-09 18:06:50 +0000

    departmental and lecturer view created

--- lib/screens/selectpage.dart
+++ lib/screens/selectpage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -19,7 +19,8 @@ class SelectPage extends StatelessWidget {
             children: [
               Container(
                 margin: const EdgeInsets.only(
-                  top: 15,
+                  top: 20,
+                  left: 30,
                 ),
                 padding: const EdgeInsets.all(30),
                 width: 350,
@@ -32,7 +33,7 @@ class SelectPage extends StatelessWidget {
                 children: [
                   Container(
                     padding: const EdgeInsets.all(25),
-                    margin: const EdgeInsets.only(top: 20),
+                    margin: const EdgeInsets.only(top: 20,left: 30),
                     width: MediaQuery.of(context).size.width / 2,
                     height: MediaQuery.of(context).size.width * 0.46,
                     decoration: BoxDecoration(
@@ -43,9 +44,9 @@ class SelectPage extends StatelessWidget {
                     ),
                   ),
                   Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: MediaQuery.of(context).size.width * 0.44,
+                    margin: const EdgeInsets.only(top: 10),
+                    // width: MediaQuery.of(context).size.width / 2,
+                    // height: MediaQuery.of(context).size.width * 0.44,
                     decoration: BoxDecoration(
                  
                     ),
@@ -53,17 +54,58 @@ class SelectPage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Container(
-                          margin: const EdgeInsets.only(left: 90),
+                          margin: const EdgeInsets.only(left: 100),
                           child: Text("Welcome",style: GoogleFonts.montserrat(textStyle: header1Bold),),
                         ),
                         Container(
-                          margin: const EdgeInsets.only(left: 90),
+                          margin: const EdgeInsets.only(left: 100),
                           child: Text("Select an option to begin...",style: GoogleFonts.montserrat(textStyle: subheaderBold),),
                         ),
                         Container(
-                          margin: const EdgeInsets.only(left: 130,top: 30),
-                          height: 450,
+                          margin: const EdgeInsets.only(left: 100,top: 20),
+                          height: 350,
                           width: 400,
+                          child: Column(
+                           
+                            children: [
+                              SizedBox(height: 70,),
+                              Container(
+                                padding: const EdgeInsets.all(10),
+
+                                child: Center(
+                                  child: Text("Lecturer ",style: GoogleFonts.montserrat(
+                                    textStyle: subheaderBoldbtn
+                                  ),),
+                                ),
+                              margin: const EdgeInsets.only(top: 30),
+                                height: 50,
+                                width: 300,
+                                decoration: BoxDecoration(
+                              color: AppColor.mainBlue,borderRadius: BorderRadius.circular(10)
+                                ),
+                          
+                              ),
+                               SizedBox(height: 30,),
+                              GestureDetector(
+                                child: Container(
+                                    padding: const EdgeInsets.all(10),
+                                  child: Center(
+                                    child: Text("Department",style: GoogleFonts.montserrat(
+                                      textStyle: subheaderBoldbtn
+                                    ),),
+                                  ),
+                                  margin: const EdgeInsets.only(bottom: 30),
+                                   height: 50,
+                                  width: 300,
+                                   decoration: BoxDecoration(
+                                color: AppColor.mainBlue,borderRadius: BorderRadius.circular(10)
+                                  ),
+                                                        
+                              
+                                ),
+                              )
+                            ],
+                          ),
                           decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(10),
@@ -75,6 +117,7 @@ class SelectPage extends StatelessWidget {
                                 offset: Offset(2, 5),
                               ),
                             ],
+                          
                           ),
                         ),
                       ],
--- lib/screens/text.dart
+++ lib/screens/text.dart
@@ -3,5 +3,6 @@ import 'package:simbackend/utils/colors.dart';
 
  TextStyle header1Bold = TextStyle(color: AppColor.mainBlue,fontSize: 48,fontWeight: FontWeight.bold);
  TextStyle header1Regular= TextStyle(color: AppColor.mainBlue,fontSize: 48,fontWeight: FontWeight.normal);
- TextStyle subheaderBold = TextStyle(color: AppColor.mainBlue,fontSize: 20,fontWeight: FontWeight.bold);
- TextStyle subheaderRegular = TextStyle(color: AppColor.mainBlue,fontSize: 24,fontWeight: FontWeight.normal);
+ TextStyle subheaderBold = TextStyle(color: AppColor.mainBlue,fontSize: 18,fontWeight: FontWeight.bold);
+ TextStyle subheaderRegular = TextStyle(color: Colors.white,fontSize: 24,fontWeight: FontWeight.normal);
+  TextStyle subheaderBoldbtn = TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.bold);
