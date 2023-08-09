4a3b180fa04f593fa0ecd8808a995f8572740559
Ampaw24 <ampawsafo22@gmail.com>
2023-08-09 14:27:23 +0000

    updated ill

---
+++ lib/screens/selectpage.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class SelectPage extends StatelessWidget {
+  const SelectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 15, ),
+              padding: const EdgeInsets.all(30),
+              width: 350,
+              height: 120,
+              decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage("assets/logo.png")),
+              
+              
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  
+                  margin: const EdgeInsets.only(top: 20),
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: MediaQuery.of(context).size.width * 0.46,
+                 
+                   decoration: BoxDecoration(
+                    
+              image: DecorationImage(image: AssetImage("assets/illustrator.png")),
+              
+              
+              ),
+                ),
+                
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: MediaQuery.of(context).size.width * 0.46,
+              
+                   decoration: BoxDecoration(
+                        color: Colors.blue,
+           
+              
+              
+              ),
+                ),
+              ],
+            )
+
+            
+          ],
+        ),
+        
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color mainBlue = Color(0xff23395D);
+ 
+
+}
