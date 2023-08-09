b36abeb8ed0b45402d24be0ca2144837460ae047
Ampaw24 <ampawsafo22@gmail.com>
2023-08-09 20:38:40 +0000

    update ui

--- lib/screens/lectureview/lecturedashboard.dart
+++ lib/screens/lectureview/lecturedashboard.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:simbackend/screens/text.dart';
 import 'package:simbackend/utils/colors.dart';
 
 class LecturerView extends StatefulWidget {
@@ -22,22 +24,79 @@ class _LecturerViewState extends State<LecturerView> {
             color: AppColor.mainBlue,
             child: Row(
            children: [
+            ImageRowText(),
+            Spacer(),
             Container(
-              padding: const EdgeInsets.all(24),
-              child: Image(image: AssetImage("assets/atulogo.png")),
+              width: 50,
+              height: 50,
+              color: Colors.white,
             ),
-            Column(
-              children: [
-                Container(child: Text("SIM")),
-                Container(child: Text("Lecturer Admin")),
-              ],
-            )
+            SizedBox(width: 20,),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.white,
+            ),
+             SizedBox(width: 20,),
+            Container(
+              margin: const EdgeInsets.only(right: 30),
+              width: 170,
+              height: 60,
+              color: Colors.white,
+              child: Column(
+                children: [
+                 Container(
+                  margin: const EdgeInsets.only(top: 7),
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage("assets/catprofile.JPG"),fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(25),
+                  color: Colors.red,
+
+                  ),
+                 ),
+                 Text("Edmund Bleo"),
+                ],
+              ),
+            ),
            ],
             ),
 
           )
         ],
       ),
+    );
+  }
+}
+
+class ImageRowText extends StatelessWidget {
+  const ImageRowText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+ Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(left: 10),
+      child: Image(image: AssetImage("assets/atulogo.png")),
+    ),
+    Column(
+      children: [
+        Container(
+          child: Text("SIM",style: GoogleFonts.montserrat(textStyle: header1boldwhite),)),
+        Container(
+          margin: const EdgeInsets.only(left: 15,),
+          child: Text("Lecturer Admin",style: GoogleFonts.montserrat(
+          textStyle: subheaderBoldWhite,
+        ),)),
+         
+      ],
+    )
+      ],
     );
   }
 }
--- lib/screens/text.dart
+++ lib/screens/text.dart
@@ -5,4 +5,6 @@ import 'package:simbackend/utils/colors.dart';
  TextStyle header1Regular= TextStyle(color: AppColor.mainBlue,fontSize: 48,fontWeight: FontWeight.normal);
  TextStyle subheaderBold = TextStyle(color: AppColor.mainBlue,fontSize: 18,fontWeight: FontWeight.bold);
  TextStyle subheaderRegular = TextStyle(color: Colors.white,fontSize: 24,fontWeight: FontWeight.normal);
-  TextStyle subheaderBoldbtn = TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.bold);
+  TextStyle subheaderBoldbtn = TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w600);
+  TextStyle header1boldwhite= TextStyle(color: Colors.white,fontSize: 48,fontWeight: FontWeight.normal);
+   TextStyle subheaderBoldWhite = TextStyle(color: Colors.white,fontSize: 15,fontWeight: FontWeight.normal);
