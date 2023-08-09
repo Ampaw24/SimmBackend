fb2b3f53bff30bcdba664f50e2fb1fcc6e31e61c
Edmund Quainoo <edmundquainoo3018@gmail.com>
2023-08-09 14:37:48 +0000

    Seen

--- lib/screens/selectpage.dart
+++ lib/screens/selectpage.dart
@@ -9,56 +9,46 @@ class SelectPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 15, ),
-              padding: const EdgeInsets.all(30),
-              width: 350,
-              height: 120,
-              decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("assets/logo.png")),
-              
-              
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  
-                  margin: const EdgeInsets.only(top: 20),
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: MediaQuery.of(context).size.width * 0.46,
-                 
-                   decoration: BoxDecoration(
-                    
-              image: DecorationImage(image: AssetImage("assets/illustrator.png")),
-              
-              
-              ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 15,
                 ),
-                
-                Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: MediaQuery.of(context).size.width * 0.46,
-              
-                   decoration: BoxDecoration(
-                        color: Colors.blue,
-           
-              
-              
-              ),
+                padding: const EdgeInsets.all(30),
+                width: 350,
+                height: 120,
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage("assets/logo.png")),
                 ),
-              ],
-            )
-
-            
-          ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: MediaQuery.of(context).size.width * 0.46,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/illustrator.png")),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: MediaQuery.of(context).size.width * 0.46,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
-        
       ),
     );
   }
